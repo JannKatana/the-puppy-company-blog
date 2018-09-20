8673e62e507125510ec96e76132443ab566a6cbe
Mel-john Catanaoan <mjtcatanaoan@gmail.com>
2018-09-20 20:06:46 +0800

    Descriptio: + Added Forms, and Views for User + Added Unit test

--- puppycompanyblog/models.py
+++ puppycompanyblog/models.py
@@ -14,7 +14,7 @@ class User(db.Model, UserMixin):
 
 	__tablename__ = 'users'
 
-	id = db.Column(db.Interger, primary_key=True)
+	id = db.Column(db.Integer, primary_key=True)
 	profile_image = db.Column(db.String(20), nullable=False, default='default_profile.png')
 	email = db.Column(db.String(64), unique=True, index=True)
 	username = db.Column(db.String(64), unique=True, index=True)
@@ -39,7 +39,7 @@ class BlogPost(db.Model):
 	users = db.relationship(User)
 
 	id = db.Column(db.Integer, primary_key=True)
-	user_id = db.Column(db.Interger, db.ForiengKey(users.id), nullable=False)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
 	date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 	title = db.Column(db.String(140), nullable=False)
